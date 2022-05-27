e61613e6e0709bd7eebd19454983fc96ae006855
Daniel-olaO <danieladedeji135@gmail.com>
2022-05-27 11:55:42 -0400

    authentication added

--- src/routes/course.js
+++ src/routes/course.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const courseController = require('../controllers/course.controls');
+const { authenticateToken } = require('../controllers/user.controls');
 const validator = require('../validations/course');
 
 const router = express.Router();
 
-router.get('/', courseController.getAllCoures);
-
-router.get('/:code', courseController.getCourseByCode);
+router.get('/',
+    authenticateToken,
+    courseController.getAllCoures
+);
+router.get('/:code',
+    authenticateToken,
+    courseController.getCourseByCode
+);
 
 router.post('/addCourse',
+    authenticateToken,
     validator.validateCourse,
     courseController.addCourse
 );
 
 router.put('/updateCourse/:code',
+    authenticateToken,
     validator.validateCourse,
     courseController.updateCourseByCode
 );
 
-router.delete('/deleteCourse/:code', courseController.deleteCourseByCode);
+router.delete('/deleteCourse/:code',
+    authenticateToken,
+    courseController.deleteCourseByCode
+);
 
 module.exports = router
--- src/routes/student.js
+++ src/routes/student.js
@@ -1,26 +1,39 @@
 const express = require('express')
 const studentController = require('../controllers/student.controls');
+const { authenticateToken } = require('../controllers/user.controls');
 const validator = require('../validations/student');
 
 const router = express.Router();
 
-router.post('/addStudent', 
+router.post('/addStudent',
+    authenticateToken,
     validator.validateStudent,
     studentController.addStudent
 );
-
-router.get('/', studentController.getStudents);
-
-router.get('/:id', studentController.getStudentById);
-
+router.get('/', 
+    authenticateToken,
+    studentController.getStudents
+);
+router.get('/:id',
+    authenticateToken,
+    studentController.getStudentById
+);
 router.put('/updateStudent/:id',
+    authenticateToken,
     validator.validateStudent,
     studentController.updateStudent
 );
-router.put('/:id/takeCourse/:code', studentController.takeCouse);
-
-router.put('/:id/dropCourse/:code', studentController.dropCouse);
-
-router.delete('/deleteStudent/:id', studentController.deleteStudentById);
+router.put('/:id/takeCourse/:code', 
+    authenticateToken,
+    studentController.takeCouse
+);
+router.put('/:id/dropCourse/:code',
+    authenticateToken,
+    studentController.dropCouse
+);
+router.delete('/deleteStudent/:id',
+    authenticateToken,
+    studentController.deleteStudentById
+);
 
 module.exports = router;
